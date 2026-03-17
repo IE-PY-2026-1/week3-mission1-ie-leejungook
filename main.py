72e03bb454b97b873cb5d8d2151b439c4057ce3e
leejungook <jungook0202@mju.ac.kr>
2026-03-17 17:44:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# 파일이름 :
-# 작 성 자 :
+# 파일이름 : 60232303 이정욱 3주차 과제 
+# 작 성 자 : 이정욱
+# 미션 1
+name3 = input("이름을 입력하시오: ")
+writing3 = int(input("당신의 글쓰기 점수를 입력하시오: "))
+python3 = int(input("당신의 파이썬 점수를 입력하시오: "))
+last_avg3 = float(input("당신의 지난학기 평균을 입력하시오: "))
+
+average3 = (writing3 + python3)/2
+diff = average3 - last_avg3
+
+print(f"{name3}, 학생의 글쓰기 점수는 {writing3},파이썬 점수는 {python3}, 입니다.")
+print(f"평균은 {average3} 이고, 지난학기와 차이는 {diff}, 입니다.")
