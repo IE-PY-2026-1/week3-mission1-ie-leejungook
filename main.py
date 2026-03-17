dfa1a2031f8b4ed92532a49c2a7cc8b57f14a3a7
leejungook <jungook0202@mju.ac.kr>
2026-03-17 17:54:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ writing3 = int(input("당신의 글쓰기 점수를 입력하시오: "))
 python3 = int(input("당신의 파이썬 점수를 입력하시오: "))
 last_avg3 = float(input("당신의 지난학기 평균을 입력하시오: "))
 
-average3 = (writing3 + python3)/2
+average3 = (writing3 + python3) / 2
 diff = average3 - last_avg3
 
-print("")
-print(f"{name3}, 학생의 글쓰기 점수는 {writing3} 파이썬 점수는 {python3} 입니다.")
-print(f"평균은 {average3} 이고 지난학기와 차이는 {diff} 입니다.")
+print("") 
+print(f"{name3} 학생의 글쓰기 점수는 {writing3}, 파이썬 점수는 {python3} 입니다.")
+print(f"평균은 {average3} 이고, 지난 학기와 차이는 {diff} 입니다.")
