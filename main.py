d2ed6604ab24ee8ee863f9e044fac1f8fa300216
leejungook <jungook0202@mju.ac.kr>
2026-03-17 17:50:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ last_avg3 = float(input("당신의 지난학기 평균을 입력하시오: "))
 average3 = (writing3 + python3)/2
 diff = average3 - last_avg3
 
-print()
+print("")
 print(f"{name3}, 학생의 글쓰기 점수는 {writing3} 파이썬 점수는 {python3} 입니다.")
 print(f"평균은 {average3} 이고 지난학기와 차이는 {diff} 입니다.")
